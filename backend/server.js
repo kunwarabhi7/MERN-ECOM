c520ba3e8702b6f744064d35ebeac75d96adf06a
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-12-26 14:39:49 +0530

    restart

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectToDB from "./utils/db.js";
 import route from "./routes/product.route.js";
-
+import cors from "cors";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
-
+app.use(cors());
 app.use("/api/product", route);
 
 app.listen(PORT, () => {
